22cfcb130ee106b51441cdfa53e80906589b0ebb
SebastianLedesma <50058025+SebastianLedesma@users.noreply.github.com>
2020-02-03 12:30:21 -0300

    Add files via upload
    Ejercicios hechos en clase.

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,14 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    //declaro una variable que almacenara el contenido del input.
+    var nombre;
+    
+    //asigno el contenido del input a la variable.
+    nombre = document.getElementById("elNombre").value;
+
+    //muestro el mensaje por ventana.
+    alert("El nombre ingresado es " + nombre);
 }
 
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -5,5 +5,13 @@
 function mostrar()
 {
 
+	//declaro una variable.
+	var nombre;
+
+	//asigno a la variable un texto por ventana emergente.
+	nombre = prompt("Ingrese un nombre:");
+
+	//asigno el valor de la ventana emergente al elemento con el id 'elNombre'.
+	document.getElementById("elNombre").value = nombre;
 }
 
