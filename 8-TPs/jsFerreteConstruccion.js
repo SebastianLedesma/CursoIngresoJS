93b45f9a6e5a840ee2b667c43f953927729bb6a0
SebastianLedesma <50058025+SebastianLedesma@users.noreply.github.com>
2020-02-17 12:33:17 -0300

    Ejercicio hecho en clase.

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,44 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var largo ;
+    var ancho;
+    var perimetro;
+
+    largo = parseFloat(document.getElementById("Largo").value);
+    ancho = parseFloat(document.getElementById("Ancho").value);
+
+    perimetro = (2* largo + 2* ancho) * 3;
+
+    alert("Cantidad de alambre para el rectangulo: " + perimetro);
 
 }
 function Circulo () 
 {
-	
+    var radio;
+    var resultado;
+
+    radio = parseFloat(document.getElementById("Radio").value);
+
+    resultado = (2 * 3.1416 * radio) * 3;
+
+    alert("Cantidad de alambre para el circulo:" + resultado);
 }
 function Materiales () 
 {
-	
+    var largo;
+    var ancho;
+    var bolsaCemento;
+    var bolsaCal;
+    var perimetro;
+
+    largo = parseFloat(document.getElementById("Largo").value);
+    ancho = parseFloat(document.getElementById("Ancho").value);
+    perimetro = (2* largo) + (2 * ancho);
+
+    bolsaCal = perimetro * 3;
+    bolsaCemento = perimetro * 2;
+
+    alert("Perimetro:"+ perimetro+"/nBolsas de Cal: "+bolsaCal+"/nBolsa de cemento: "+ bolsaCemento);
+
 }
--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,14 +1,15 @@
 /*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
-A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un porcDescuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un porcDescuento del 40 % y si es de otra marca el porcDescuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un porcDescuento del 25 % y si es de otra marca el porcDescuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el porcDescuento es del 15%, si es  “FelipeLamparas” se hace un porcDescuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con porcDescuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
 function CalcularPrecio() {
+    /*
     var cantidad;
     var marca;
     var resultado;
@@ -55,5 +56,68 @@ function CalcularPrecio() {
     }
 
      document.getElementById("precioDescuento").value = resultado;
+     */
+
+    var cantidad;
+    var marca;
+    var porcDescuento;
+    var descuento;
+    var precio = 35;
+    var precioDescuento;
+    var importeFinal;
+    var iibb;
+
+    cantidad = parseInt(document.getElementById("Cantidad").value);
+    marca = document.getElementById("Marca").value;
+
+    switch (cantidad) {
+        case 1:
+        case 2:
+            porcDescuento = 0;
+            break;
+        case 3:
+            if (marca == "ArgentinaLuz") {
+                porcDescuento = 15;
+            } else if (marca == "FelipeLamparas") {
+                porcDescuento = 10;
+            } else {
+                porcDescuento = 5;
+            }
+            break;
+        case 4:
+            if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+                porcDescuento = 25;
+            } else {
+                porcDescuento = 20;
+            }
+            break;
+        case 5:
+            if (marca == "ArgentinaLuz") {
+                porcDescuento = 40;
+            } else {
+                porcDescuento = 30;
+            }
+            break;
+        default:
+            porcDescuento = 50;
+    }
+
+    //calculo el descuento.
+    descuento = precio * porcDescuento / 100;
+
+    //calculo el precio con descuento de 1 lampara,
+    precioDescuento = precio - descuento;
+
+    document.getElementById("precioDescuento").value = precioDescuento;
+
+    importeFinal = precioDescuento * cantidad;
+
+    if (importeFinal >= 120) {
+        iibb = importeFinal * 10 / 100;
+        importeFinal = importeFinal + iibb;
+        alert("El importe final es $"+ importeFinal+ " \nIngresosBrutos.Usted pagó "+iibb);
+    }else{
+        alert("El importe final es "+ importeFinal);
+    }
 
 }
